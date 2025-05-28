082388b74a7c8e9087a40dcf20df947dca1be9f5
devmini1203 <devmini1203@gmail.com>
2025-05-28 22:22:30 +0900

    Revert "chore: class 5.12 complete"
    This reverts commit d52ca016cfd09eccfced371aa274680e63d8cd93.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import Register from "./components/Register";
+<<<<<<< HEAD
+=======
+import HookExam from "./components/HookExam";
+>>>>>>> parent of d52ca01 (chore: class 5.12 complete)
 
 function App() {
   return (
     <>
+<<<<<<< HEAD
       <Register />
+=======
+      <HookExam />
+>>>>>>> parent of d52ca01 (chore: class 5.12 complete)
     </>
   );
 }
