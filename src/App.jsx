8d499cc0def9cb684a56caa48eaf22bd03c4f27a
devmini1203 <devmini1203@gmail.com>
2025-05-28 22:26:25 +0900

    chore: class 6.1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import "./App.css";
-import Register from "./components/Register";
-<<<<<<< HEAD
-=======
-import HookExam from "./components/HookExam";
->>>>>>> parent of d52ca01 (chore: class 5.12 complete)
 
 function App() {
-  return (
-    <>
-<<<<<<< HEAD
-      <Register />
-=======
-      <HookExam />
->>>>>>> parent of d52ca01 (chore: class 5.12 complete)
-    </>
-  );
+  return <>카운터앱</>;
 }
 
 export default App;
